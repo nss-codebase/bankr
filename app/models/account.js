71094e21de0b16a3668903dc3dba6fc29539e0a4
Chyld Medford <chyld.medford@gmail.com>
2014-08-08 20:06:54 -0500

    models really done

--- app/models/account.js
+++ app/models/account.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var Mongo = require('mongodb');
+var Transaction = require('./transaction');
+var Transfer    = require('./transfer');
+var Mongo       = require('mongodb');
+var _           = require('lodash');
 
 function Account(o){
   this.name    = o.name;
@@ -27,7 +30,48 @@ Account.all = function(cb){
 
 Account.findById = function(id, cb){
   var _id = Mongo.ObjectID(id);
-  Account.collection.findOne({_id:_id}, cb);
+  Account.collection.findOne({_id:_id}, function(err, obj){
+    cb(err, _.create(Account.prototype, obj));
+  });
+};
+
+Account.prototype.addTransaction = function(o, cb){
+  if(o.pin !== this.pin){cb(); return;}
+
+  o.accountId = this._id;
+  o.fee = 0;
+
+  if(o.type === 'deposit'){
+    this.balance += parseFloat(o.amount);
+  }else{
+    this.balance -= parseFloat(o.amount);
+    if(this.balance < 0){this.balance -= 50; o.fee = 50;}
+  }
+
+  Account.collection.save(this, function(){
+    Transaction.create(o, cb);
+  });
+};
+
+Account.prototype.transfer = function(o, cb){
+  if(o.pin !== this.pin){cb(); return;}
+
+  var self = this;
+  o.fromId = self._id;
+  o.fee = 25;
+  o.amount = parseFloat(o.amount);
+
+  if(self.balance >= (o.amount + o.fee)){
+    Account.findById(o.toId, function(err, receiver){
+      self.balance -= (o.amount + o.fee);
+      receiver.balance += o.amount;
+      Account.collection.save(self, function(){
+        Account.collection.save(receiver, function(){
+          Transfer.create(o, cb);
+        });
+      });
+    });
+  }
 };
 
 module.exports = Account;
--- test/unit/account.js
+++ test/unit/account.js
@@ -61,8 +61,93 @@ describe('Account', function(){
   describe('.findById', function(){
     it('should find an account by its id', function(done){
       Account.findById('000000000000000000000001', function(err, account){
+        expect(account).to.be.instanceof(Account);
         expect(account.name).to.equal('Bob Jones');
         done();
+      });
+    });
+  });
+
+  describe('#transaction', function(){
+    it('should create a transaction - deposit', function(done){
+      Account.findById('000000000000000000000001', function(err, account){
+        account.addTransaction({type:'deposit', pin:'1234', amount:'100'}, function(err, transaction){
+          expect(account.balance).to.equal(8100);
+          expect(transaction.date).to.be.instanceof(Date);
+          expect(transaction.type).to.equal('deposit');
+          expect(transaction.amount).to.equal(100);
+          expect(transaction.fee).to.equal(0);
+          expect(transaction.accountId).to.deep.equal(Mongo.ObjectID('000000000000000000000001'));
+          done();
+        });
+      });
+    });
+
+    it('should not create a transaction - bad pin', function(done){
+      Account.findById('000000000000000000000001', function(err, account){
+        account.addTransaction({type:'deposit', pin:'2341', amount:'100'}, function(err, transaction){
+          expect(transaction).to.be.undefined;
+          done();
+        });
+      });
+    });
+
+    it('should create a transaction - withdraw', function(done){
+      Account.findById('000000000000000000000001', function(err, account){
+        account.addTransaction({type:'withdraw', pin:'1234', amount:'100'}, function(err, transaction){
+          expect(account.balance).to.equal(7900);
+          expect(transaction.amount).to.equal(100);
+          expect(transaction.fee).to.equal(0);
+          done();
+        });
+      });
+    });
+
+    it('should create a transaction - withdraw with fee', function(done){
+      Account.findById('000000000000000000000001', function(err, account){
+        account.addTransaction({type:'withdraw', pin:'1234', amount:'10000'}, function(err, transaction){
+          expect(account.balance).to.equal(-2050);
+          expect(transaction.amount).to.equal(10000);
+          expect(transaction.fee).to.equal(50);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('#transfer', function(){
+    it('should transfer money - good - enough funds', function(done){
+      Account.findById('000000000000000000000001', function(err, sender){
+        sender.transfer({toId:'000000000000000000000002', pin:'1234', amount:'100'}, function(err, transfer){
+          Account.findById('000000000000000000000002', function(err, receiver){
+            expect(sender.balance).to.equal(7875);
+            expect(receiver.balance).to.equal(5100);
+            expect(transfer.date).to.be.instanceof(Date);
+            expect(transfer.amount).to.equal(100);
+            expect(transfer.fee).to.equal(25);
+            expect(transfer.fromId).to.deep.equal(Mongo.ObjectID('000000000000000000000001'));
+            expect(transfer.toId).to.deep.equal(Mongo.ObjectID('000000000000000000000002'));
+            done();
+          });
+        });
+      });
+    });
+
+    it('should transfer money - bad - bad pin', function(done){
+      Account.findById('000000000000000000000001', function(err, sender){
+        sender.transfer({toId:'000000000000000000000002', pin:'1231', amount:'100'}, function(err, transfer){
+          expect(transfer).to.be.undefined;
+          done();
+        });
+      });
+    });
+
+    it('should transfer money - bad - not enough money', function(done){
+      Account.findById('000000000000000000000001', function(err, sender){
+        sender.transfer({toId:'000000000000000000000002', pin:'1231', amount:'100000'}, function(err, transfer){
+          expect(transfer).to.be.undefined;
+          done();
+        });
       });
     });
   });
